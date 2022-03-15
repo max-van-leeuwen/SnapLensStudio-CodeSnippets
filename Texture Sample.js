5a65a8f902197a1cc3df5609077fd92a90e6cab0
Max van Leeuwen <max@gospooky.com>
2022-03-15 21:29:40 +0100

    no message

--- Texture Sample.js
+++ Texture Sample.js
@@ -1,19 +1,35 @@
 // Max van Leeuwen
 //
 // Sample color from texture
-//
-// Make a new class instance using
-//	var sampler = new TextureSample()
+
+
+
+// Make new class instance using
+//	new TextureSample()
 //
 // Initialize a texture using
-//	sampler.setTexture(<Asset.Texture>)
+//	.setTexture(<Asset.Texture>)
 //
-// Then, sample a coordinate on the texture (between 0-1) using
-//	sampler.sample(<vec2>)
+// Sample using
+//	.sample(<vec2>)
+//
+// Alternatively, use this imple one-liner if you only need to sample a value once
+//	var color = new TextureSample( <texture asset> ).sample( <position vec2> )
 
 
 
-global.TextureSample = function(){
+global.TextureSample = function(textureToSample){
+	// private
+	var data;
+	var tex;
+	var dimensions = {"w":null, "h":null};
+
+	function clamp(v){
+		return v < 0 ? 0 : v > 1 ? 1 : v;
+	}
+
+	
+	// public
 	/**
 	 * @type {Function}
 	 * @description Change the texture to sample. */
@@ -23,20 +39,16 @@ global.TextureSample = function(){
 		dimensions.w = tex.getWidth() - 1;
 		dimensions.h = tex.getHeight() - 1;
 	}
+	if(textureToSample) this.setTexture(textureToSample); // set texture if passed in as argument
 
 	/**
 	 * @type {Function}
 	 * @description Sample at texture coordinate (vec2, 0-1). Returns vec4 color (8bit). */
 	this.sample = function(pos){
 		tex.control.getPixels(
-			Math.round(pos.x * dimensions.w),
-			Math.round(pos.y * dimensions.h), 1, 1, data
+			Math.round(clamp(pos.x) * dimensions.w),
+			Math.round(clamp(pos.y) * dimensions.h), 1, 1, data
 		);
 		return new vec4(data[0], data[1], data[2], data[3]).uniformScale(1/255);
 	}
-
-	// private
-	var data;
-	var tex;
-	var dimensions = {"w":null, "h":null};
 }
