34844bd91c6038da56b7fed6456c446c5e5c4f1c
Max van Leeuwen <>
2022-12-28 01:01:11 +0100

    no message

--- Mesh Copy/Mesh Copy.js
+++ Mesh Copy/Mesh Copy.js
@@ -4,15 +4,22 @@
 // Returns the MeshBuilder object.
 //
 // How to use:
-//	global.makeMeshCopy( <Mesh Asset [Asset.RenderMesh]>, <Normals [bool] (optional, default True)>, <UVs [bool] (optional, default True)> )
+//	global.makeMeshCopy( <meshAsset [Asset.RenderMesh]>, <doNormals [bool] (optional, default True)>, <doUVs [bool] (optional, default True)>, <createEmptyUVs [bool] (optional, default False) )
+//		meshAsset			Which mesh asset to clone.
+//		doNormals			If enabled, normals will be read from mesh asset and written to new mesh copy.
+//		doUVs				If enabled, UVs will be read from mesh asset and written to new mesh copy.
+//		createEmptyUVs		If enabled, the mesh will have UVs but they will all be 0. This is useful if you're planning on overwriting the UVs later. This will overrule the 'doUVs' parameter.
 //
 // Example:
-//	var newMeshBuilder = global.makeMeshCopy(script.renderMesh, true, true); // copies normals and UVs too
+//	var newMeshBuilder = global.makeMeshCopy(script.renderMesh, true, false); // copies mesh, including normals but without UVs
 //	script.renderMeshVisual.mesh = newMeshBuilder.getMesh();
+//
+// Troubleshooting:
+//	If you get the error 'Unknown attribute name', check if you're trying to copy Normals or UVs from a mesh asset that doesn't have one of these attributes in it!
 
 
 
-global.makeMeshCopy = function(meshAsset, doNormals, doUVs){
+global.makeMeshCopy = function(meshAsset, doNormals, doUVs, createEmptyUVs){
 	var positions;
 	var normals;
 	var uvs;
@@ -24,9 +31,9 @@ global.makeMeshCopy = function(meshAsset, doNormals, doUVs){
 	if(doUVs) uvs = meshAsset.extractVerticesForAttribute("texture0");
 	
 	// prepare MeshBuilder data
-	var dataTypes = [				{name:"position", components:3}	]
-	if(doNormals) dataTypes.push(	{name:"normal", components:3}	);
-	if(doUVs) dataTypes.push(		{name:"texture0", components:2}	);
+	var dataTypes = [								{name:"position", components:3}	]
+	if(doNormals) dataTypes.push(					{name:"normal", components:3}	);
+	if(doUVs || createEmptyUVs) dataTypes.push(		{name:"texture0", components:2}	);
 
 	// new MeshBuilder object
 	var builder = new MeshBuilder(dataTypes);
@@ -46,9 +53,9 @@ global.makeMeshCopy = function(meshAsset, doNormals, doUVs){
 			vertices.push(normals[vtx3+1]);
 			vertices.push(normals[vtx3+2]);
 		}
-		if(doUVs){
-			vertices.push(uvs[vtx2+0]);
-			vertices.push(uvs[vtx2+1]);
+		if(doUVs || createEmptyUVs){
+			vertices.push(createEmptyUVs ? 0 : uvs[vtx2+0]);
+			vertices.push(createEmptyUVs ? 0 : uvs[vtx2+1]);
 		}
 	}
 
